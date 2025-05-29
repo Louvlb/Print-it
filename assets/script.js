6377f876dc8e89d9f1138198c787328cad17af93
Lou Villoing-Le Baron <louvlb05@gmail.com>
2025-05-30 01:38:33 +0200

    Ajout de commentaires

--- assets/script.js
+++ assets/script.js
@@ -17,67 +17,87 @@ const slides = [
 	}
 ]
 
-
+//Récupère la flèche gauche
 const arrow_left = document.querySelector('.arrow_left');
+
+//Récupère la flèche droite
 const arrow_right = document.querySelector('.arrow_right');
+
+//Récupère l'image du slider
 const banner_img = document.querySelector('.banner-img');
+
+//Récupère le texte du slider
 const banner_p = document.querySelector('#banner p');
 
-/*Compte le nombre de slide*/
-
+//Compte le nombre de slide
 const nbslides = slides.length;
 
+//Initialise i
 let i = 0;
 
-/*Écoute du click sur la flèche gauche*/
+//Récupère la div "dots"
+let dots = document.querySelector('.dots');
+
+//Initialise le compteur
+let compteur=0;
+
+/*Création de bullet point*/
+while (compteur<nbslides){
+	compteur++;
+	//Crée une balise p
+	let dot = document.createElement("p");
+	//L'ajoute dans la balise div class=dots
+	dots.appendChild(dot);
+	//Lui ajoute la class dot
+	dot.classList.add("dot");
+	//Lui ajoute la class dot_x
+	dot.classList.add("dot_"+[compteur]);
+}
+
+/*Création de l'effet de slider à gauche*/
 
 arrow_left.addEventListener('click', () => {
-
+	//Décompte de i
 	i--;
+	//Création de la limite de i
 	if (i < 0) {
 		i = slides.length - 1;
 	}
 	console.log(i);
 
+	//Concaténation de la src de l'image
 	let src_image = './assets/images/slideshow/' + slides[i].image;
 	console.log(src_image);
+	//Remplacementr de la src de l'image
 	banner_img.setAttribute("src", src_image);
 
+	//Récupération du texte dans une variable
 	let tagLine = slides[i].tagLine;
 	console.log(tagLine);
+	//Remplacement du texte
 	banner_p.innerHTML = tagLine;
 })
 
-/*Écoute du click sur la flèche droite*/
+/*Création de l'effet de slider à droite*/
 
 arrow_right.addEventListener('click', () => {
+	//Décompte de i
 	i++;
+	//Création de la limite de i
 	if (i >= slides.length) {
 		i = 0;
 	}
 	console.log(i);
 
+	//Concaténation de la src de l'image
 	let src_image = './assets/images/slideshow/' + slides[i].image;
 	console.log(src_image);
+	//Remplacementr de la src de l'image
 	banner_img.setAttribute("src", src_image);
 
+	//Récupération du texte dans une variable
 	let tagLine = slides[i].tagLine;
 	console.log(tagLine);
+	//Remplacement du texte
 	banner_p.innerHTML = tagLine;
 })
-
-/*Récupère la div "dots"*/
-
-let dots = document.querySelector('.dots');
-
-/*Créer les bullets points*/
-
-let compteur=0;
-while (compteur<nbslides){
-	compteur++;
-	let dot = document.createElement("p");
-	dots.appendChild(dot);
-	dot.classList.add("dot");
-	dot.classList.add("dot_"+[compteur]);
-}
-
